b3618b1170876ac43d5020cbf44f6d7b66940824
Demis Bellot <demis.bellot@gmail.com>
2020-08-07 17:11:19 +0800

    Update publish info

--- scripts/publish-app.bat
+++ scripts/publish-app.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Add GitHub Gist AccessToken GITHUB_TOKEN Environment variable or use alt variable with: -token %VAR_WITH_TOKEN%
-IF EXIST deploy\.publish COPY deploy\.publish ..\dist
+COPY deploy\* ..\dist
 PUSHD ..\dist
 x publish
 POPD
